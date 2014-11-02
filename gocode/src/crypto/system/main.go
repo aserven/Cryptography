bfc6a7be698c0f06c01ea7aadfda328e1f49cf00
AlbertServen <albert.serven@est.fib.upc.edu>
2014-11-02 21:40:01 +0100

    encrypting decrypting working

--- gocode/src/crypto/system/main.go
+++ gocode/src/crypto/system/main.go
@@ -13,6 +13,7 @@ import (
     "inout"
     "encoding/binary"
     "bytes"
+    //"strings"
 )
 
 func pkcs7PAD(file []byte, blocksize int) []byte {
@@ -46,32 +47,18 @@ func pkcs7PAD(file []byte, blocksize int) []byte {
 }
 
 func pkcs7UNPAD(file []byte, blocksize int) []byte {
-    
-    var text string = "YELLOW SUBMARINE"
-    var text2 string = "YELLOW SUBMA"
-    var t []byte = []byte(text)
-    var t2 []byte = []byte(text2)
-        
-    fmt.Println(t)
-    fmt.Printf("%# x\n",t2)
+  
+  fmt.Println(file[len(file)-20:])
 
-
-    var p int = blocksize - len(file)%blocksize
-    
-    fmt.Printf("Valor: %d  -- Valor hexa: ",p)
-    fmt.Printf("%# x\n",p)
-    
-    b := make([]byte, 2)
-    binary.LittleEndian.PutUint16(b, uint16(p))
-    
-    b[1] = b[0]
-
-    b = bytes.Repeat(b,p)
-    
-    fmt.Println(b)
-    fmt.Printf("%# x\n",b[len(b)/2:])
-    
-    return append(file,b[len(b)/2:]...)
+  b := make([]byte,2)
+  
+  b[0] = file[len(file)-1]
+  b[1] = 0
+  
+  x := int(binary.LittleEndian.Uint16(b))
+  
+  fmt.Println(x)
+  return file[:len(file)-x]
     
 }
 
@@ -79,6 +66,7 @@ func main() {
 
     filePtr := flag.String("file", "FILE", "File to encrypt/decrypt")
     keyPtr  := flag.String("key", "KEY", "Key to encrypt/decrypt")
+    namePtr := flag.String("name", "NAME", "Name of the file to write")
     //This declares numb and fork flags, using a similar approach to the word flag.
     //numbPtr := flag.Int("numb", 42, "an int")
     encPtr := flag.Bool("e", false, "Tell the program to encrypt the file")
@@ -109,9 +97,9 @@ func main() {
   key := inout.ReadFile(keyName) //[]byte("example key 1234")
   plaintext := inout.ReadFile(fileName) //[]byte("exampleplaintext")
 
-  plaintext = pkcs7PAD(plaintext,16)
+  plaintext = pkcs7PAD(plaintext,aes.BlockSize)
 
-  //fmt.Printf("To encrypt > %s == %# x\n", plaintext, plaintext)
+  fmt.Printf("To encrypt > %# x\n %# x\n", plaintext[:20], plaintext[len(plaintext)-20:])
 
   // CBC mode works on blocks so plaintexts may need to be padded to the
   // next whole block. For an example of such padding, see
@@ -134,7 +122,7 @@ func main() {
     panic(err)
   }
 
-  fmt.Println(iv)
+  fmt.Printf("IV -- %# x\n ",iv)
 
   mode := cipher.NewCBCEncrypter(block, iv)
   mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
@@ -143,12 +131,18 @@ func main() {
   // (i.e. by using crypto/hmac) as well as being encrypted in order to
   // be secure.
 
-  //fmt.Printf("Encrypted %# x\n", ciphertext)
+  inout.WriteFile(ciphertext, "prova"+ ".enc")
+
+  fmt.Printf("Encrypted %# x\n %# x\n", ciphertext[:20], ciphertext[len(ciphertext)-20:])
 
   //key := []byte("example key 1234")
   //ciphertext2, _ := hex.DecodeString("f363f3ccdcb12bb883abf484ba77d9cd7d32b5baecb3d4b1b3e0e4beffdb3ded")
-  ciphertext2 := ciphertext;
+  
+  //ciphertext2 := ciphertext;
+  ciphertext2 := inout.ReadFile(fileName+".enc");
   //fmt.Printf("To decrypt %# x\n", ciphertext2)
+
+  iv2 := ciphertext2[:aes.BlockSize]
 
   block2, err2 := aes.NewCipher(key)
   if err2 != nil {
@@ -168,7 +162,7 @@ func main() {
     panic("ciphertext2 is not a multiple of the block size")
   }
 
-  mode2 := cipher.NewCBCDecrypter(block2, iv)
+  mode2 := cipher.NewCBCDecrypter(block2, iv2)
 
   // CryptBlocks can work in-place if the two arguments are the same.
   mode2.CryptBlocks(ciphertext2, ciphertext2)
@@ -181,5 +175,11 @@ func main() {
   // using crypto/hmac) before being decrypted in order to avoid creating
   // a padding oracle.
 
-  //fmt.Printf("Decrypted: %# x\n", ciphertext2)
+  fmt.Printf("Decrypted: %# x\n %# x\n", ciphertext2[:20], ciphertext2[len(ciphertext2)-20:])
+
+  decrypted := pkcs7UNPAD(ciphertext2,aes.BlockSize)
+  fmt.Printf("%# x\n", decrypted[len(decrypted)-20:])
+
+  // inout.WriteFile(ciphertext2, strings.TrimSuffix(fileName,".enc"))
+  inout.WriteFile(ciphertext2, "prova.jpg")
 }
--- gocode/src/inout/inout.go
+++ gocode/src/inout/inout.go
@@ -4,6 +4,7 @@ import (
     "bufio"
     "io"
     "os"
+    "bytes"
 )
 
 func ReadFile(fileToOpen string) []byte {
@@ -63,4 +64,50 @@ func ReadFile(fileToOpen string) []byte {
     }*/
 
     return fileToCode
+}
+
+func WriteFile(fileToWrite []byte, fileName string) {
+
+    // make a read buffer
+    r := bytes.NewReader(fileToWrite)
+
+    // open output file
+    
+    fo, err := os.Create(fileName)
+    if err != nil {
+        panic(err)
+    }
+    // close fo on exit and check for its returned error
+    defer func() {
+        if err := fo.Close(); err != nil {
+            panic(err)
+        }
+    }()
+    // make a write buffer
+    w := bufio.NewWriter(fo)
+    
+    // make a buffer to keep chunks that are read
+    buf := make([]byte, 1024)
+    var fileToCode []byte
+    for {
+        // read a chunk
+        n, err := r.Read(buf)
+        if err != nil && err != io.EOF {
+            panic(err)
+        }
+        if n == 0 {
+            break
+        }
+        
+        // write a chunk
+        if _, err := w.Write(buf[:n]); err != nil {
+            panic(err)
+        }
+        fileToCode = append(fileToCode, buf[:n]...)
+
+    }
+
+    if err = w.Flush(); err != nil {
+        panic(err)
+    }
 }
